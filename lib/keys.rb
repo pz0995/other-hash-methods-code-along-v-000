2c60564371c3413ed9ee0371dd20f2f713eee43c
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-22 07:34:36 +0000

    automatically backed up by learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -1,16 +1,17 @@
 require 'pry'
 
-#This is the groceries hash we'll be passing in to the get_the_keys method
-# groceries = {
-#  dairy: ["milk", "yogurt", "cheese"],
-#  vegetable: ["carrots", "broccoli", "cucumbers"],
-#  meat: ["chicken", "steak", "salmon"],
-#  grains: ["rice", "pasta"]
-# }
+This is the groceries hash well be passing in to the get_the_keys method
+groceries = {
+ dairy: ["milk", "yogurt", "cheese"],
+ vegetable: ["carrots", "broccoli", "cucumbers"],
+ meat: ["chicken", "steak", "salmon"],
+ grains: ["rice", "pasta"]
+}
 
 def get_the_keys(groceries)
   groceries = {fruit: "Banana", vegetable: "Broccoli", dessert: "Cookie"}
 
   groceries.keys
+
   #  => [:fruit, :vegetable, :dessert]
 end
