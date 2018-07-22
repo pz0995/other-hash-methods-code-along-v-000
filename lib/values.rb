ed8ed067d12116d9ed07e8c67dc19a10303c4d7e
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-22 07:33:14 +0000

    automatically backed up by learn

--- lib/values.rb
+++ lib/values.rb
@@ -9,5 +9,10 @@ require 'pry'
 # }
 
 def get_the_values(groceries)
-  #code your solution here!
+  groceries = {fruit: "Banana", vegetable: "Broccoli", dessert: "Cookie"}
+
+  groceries.values
+  groceries.values.flatten
+  groceries.values.flatten.min
+  #  => ["Banana", "Broccoli", "Cookie"]
 end
